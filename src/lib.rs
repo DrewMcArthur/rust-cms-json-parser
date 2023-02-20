3612a1a77914c112cd2ea1b18bd73992aa5a1548
DrewMcArthur <DrewMcArthur1@gmail.com>
2023-02-20 12:18:57 -0700

    update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,16 @@
-pub mod filtered_in_network_file;
-pub mod in_network_file_dto;
-pub mod node_filters;
+mod filtered_in_network_file;
+mod in_network_file_dto;
+mod node_filters;
+
+// use crate::node_filters::NodeFilters;
+use crate::in_network_file_dto::InNetworkFile;
+
+pub fn get_filtered_in_network_file(
+    bytes: &[u8],
+    // filters: &NodeFilters,
+) -> String {
+    serde_json::to_string(
+        &serde_json::from_slice::<InNetworkFile>(bytes).expect("valid InNetworkFile json")
+    )
+    .expect("validly deserialized InNetworkFile")
+}
