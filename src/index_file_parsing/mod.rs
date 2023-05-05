6e81c98a537f06d0861ed4b0af49bffe6ed15865
drewmcarthur <drewmcarthur1@gmail.com>
2023-05-05 14:41:45 -0400

    extract handle_reporting_structure

--- src/index_file_parsing/mod.rs
+++ src/index_file_parsing/mod.rs
@@ -2,7 +2,11 @@ use std::fs::File;
 
 use crate::index_file_parsing::{index_file::IndexFile, meta_repository_trait::DbLinkInput};
 
-use self::meta_repository_trait::{FileRowInput, MetaRepository, PlanInput};
+use self::{
+    csv_meta_repository::CsvMetaRepository,
+    index_file::ReportingStructure,
+    meta_repository_trait::{FileRowInput, MetaRepository, PlanInput},
+};
 
 pub mod csv_meta_repository;
 pub mod index_file;
@@ -17,11 +21,7 @@ pub fn parse_index_file(path: &str) {
     let file = File::open(path).unwrap();
     let index_file: IndexFile = serde_json::from_reader(file).unwrap();
 
-    let repo = csv_meta_repository::CsvMetaRepository {
-        files_csv_path: "./db/files.csv",
-        links_csv_path: "./db/links.csv",
-        plans_csv_path: "./db/plans.csv",
-    };
+    let repo = _get_repo();
 
     let index_file_id = repo.add_file(&mut FileRowInput {
         url: path,
@@ -31,46 +31,69 @@ pub fn parse_index_file(path: &str) {
     });
 
     for node in index_file.reporting_structure {
-        let mut plan_ids: Vec<usize> = vec![];
-        let mut file_ids: Vec<usize> = vec![];
+        handle_reporting_structure(
+            index_file_id,
+            &index_file.reporting_entity_name,
+            &index_file.reporting_entity_type,
+            &node,
+        );
+    }
+}
 
-        for plan in node.reporting_plans {
-            plan_ids.push(repo.add_plan(&mut PlanInput::from_reporting_plan(&plan)));
-        }
+fn handle_reporting_structure(
+    index_file_id: usize,
+    reporting_entity_name: &str,
+    reporting_entity_type: &str,
+    node: &ReportingStructure,
+) {
+    let repo = _get_repo();
+    let mut plan_ids: Vec<usize> = vec![];
+    let mut file_ids: Vec<usize> = vec![];
 
-        for rate_file in node.in_network_files {
-            file_ids.push(repo.add_file(&mut FileRowInput {
-                url: &rate_file.location,
-                filename: _get_filename_from_url(&rate_file.location).as_str(),
-                reporting_entity_name: &index_file.reporting_entity_name,
-                reporting_entity_type: &index_file.reporting_entity_type,
-            }));
-        }
+    for plan in node.reporting_plans.as_slice() {
+        plan_ids.push(repo.add_plan(&mut PlanInput::from_reporting_plan(&plan)));
+    }
 
+    for rate_file in node.in_network_files.as_slice() {
         file_ids.push(repo.add_file(&mut FileRowInput {
-            url: &node.allowed_amount_file.location,
-            filename: &node.allowed_amount_file.description,
-            reporting_entity_name: &index_file.reporting_entity_name,
-            reporting_entity_type: &index_file.reporting_entity_type,
+            url: &rate_file.location,
+            filename: _get_filename_from_url(&rate_file.location).as_str(),
+            reporting_entity_name: reporting_entity_name,
+            reporting_entity_type: reporting_entity_type,
         }));
+    }
 
-        for file_id in &file_ids {
+    file_ids.push(repo.add_file(&mut FileRowInput {
+        url: &node.allowed_amount_file.location,
+        filename: &node.allowed_amount_file.description,
+        reporting_entity_name: reporting_entity_name,
+        reporting_entity_type: reporting_entity_type,
+    }));
+
+    for file_id in &file_ids {
+        repo.add_link(&mut DbLinkInput {
+            from_id: index_file_id.clone(),
+            from_type: "index_file",
+            to_id: file_id.clone(),
+            to_type: "rate_file",
+        });
+
+        for plan_id in &plan_ids {
             repo.add_link(&mut DbLinkInput {
-                from_id: index_file_id.clone(),
-                from_type: "index_file",
+                from_id: plan_id.clone(),
+                from_type: "plan",
                 to_id: file_id.clone(),
                 to_type: "rate_file",
             });
-
-            for plan_id in &plan_ids {
-                repo.add_link(&mut DbLinkInput {
-                    from_id: plan_id.clone(),
-                    from_type: "plan",
-                    to_id: file_id.clone(),
-                    to_type: "rate_file",
-                });
-            }
         }
+    }
+}
+
+fn _get_repo() -> CsvMetaRepository<'static> {
+    csv_meta_repository::CsvMetaRepository {
+        files_csv_path: "./db/files.csv",
+        links_csv_path: "./db/links.csv",
+        plans_csv_path: "./db/plans.csv",
     }
 }
 
